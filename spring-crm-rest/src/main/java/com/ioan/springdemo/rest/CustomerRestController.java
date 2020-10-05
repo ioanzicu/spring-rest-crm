428fdede069587d6e7fbc14e14c3c28fbcaa7b71
Ioan <ioan.zicu94gmail.com>
2020-10-05 23:14:44 +0200

    PUT customer

--- spring-crm-rest/src/main/java/com/ioan/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest/src/main/java/com/ioan/springdemo/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,13 +43,21 @@ public class CustomerRestController {
 	}
 
 	// add mapping for POST /customers - add new customer
-
 	@PostMapping("/customers")
 	public Customer addCustomer(@RequestBody Customer theCustomer) {
 
 		// pass an id in JSON ... set id to 0 to force a save of new item,
 		// no update
 		theCustomer.setId(0);
+
+		customerService.saveCustomer(theCustomer);
+
+		return theCustomer;
+	}
+
+	// add mapping for PUT /customers - update existing customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
 
 		customerService.saveCustomer(theCustomer);
 
